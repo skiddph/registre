0c61c6f491c346d7f1e9dd15302d5ac9ba5de2fd
eru123 <yeoligoakino@gmail.com>
2022-03-01 20:04:21 +0800

    added fontawesome icon

--- src/main.js
+++ src/main.js
@@ -6,8 +6,16 @@ import VueQrcodeReader from "qrcode-reader-vue3";
 
 import '@/styles/global.scss'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCamera, faCameraRotate, faLightbulb } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faCamera)
+library.add(faCameraRotate)
+library.add(faLightbulb)
+
 const app = createApp(App)
 app.use(router)
 app.use(store)
 app.use(VueQrcodeReader)
+app.component('icon', FontAwesomeIcon)
 app.mount('#app')
