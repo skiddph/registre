b7152116a2bfc7660086fed6c0438995cb88e2c6
eru123 <yeoligoakino@gmail.com>
2022-03-01 20:03:16 +0800

    added legends

---
+++ src/components/Legends.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="false" class="legends">
+    <h3>Legends</h3>
+    <div>
+      <Indicator state="ready" />- Ready
+    </div>
+    <div>
+      <Indicator state="loading" />- Detected
+    </div>
+    <div>
+      <Indicator state="in" />- Employee In
+    </div>
+    <div>
+      <Indicator state="out" />- Employee Out
+    </div>
+    <div>
+      <Indicator state="error" />- Error
+    </div>
+  </div>
+</template>
