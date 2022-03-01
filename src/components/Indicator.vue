90d6a23d488eae904edbc9f3977aedf6906a9d6f
eru123 <yeoligoakino@gmail.com>
2022-03-01 22:59:25 +0800

    added large props

--- src/components/Indicator.vue
+++ src/components/Indicator.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="`indicator ${state}`" />
+  <div :class="`indicator ${state}${large ? ' large' : ''}`" />
 </template>
 <script>
 export default {
@@ -8,6 +8,41 @@ export default {
       type: String,
       default: 'loading',
     },
+    large: {
+      type: Boolean,
+      default: false,
+    },
   },
 }
 </script>
+<style lang="scss" scoped>
+.indicator {
+  @apply p-1 rounded-full m-4 border inline-block;
+  border-color: rgba(255,255,255,0);
+
+  &.large {
+    @apply p-2;
+  }
+
+  &.ready {
+    background-color: #00ff00;
+  }
+
+  &.error {
+    background-color: #ff0000;
+  }
+
+  &.in {
+    background-color: #0080ff;
+  }
+
+  &.out {
+    background-color: #1b1b1b;
+    border-color: rgba(255,255,255,1);
+  }
+
+  &.loading {
+    background-color: #dd8533;
+  }
+}
+</style>
