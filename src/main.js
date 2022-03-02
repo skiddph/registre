3a91169ed7713264c3fd77c7f791ab18f6a4b6ac
eru123 <yeoligoakino@gmail.com>
2022-03-02 17:41:12 +0800

    added data icons

--- src/main.js
+++ src/main.js
@@ -7,12 +7,15 @@ import VueQrcodeReader from "qrcode-reader-vue3";
 import '@/styles/global.scss'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCamera, faCameraRotate, faLightbulb, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { faCamera, faCameraRotate, faLightbulb, faSpinner, faFileImport, faFileExport,faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faCamera)
 library.add(faCameraRotate)
 library.add(faLightbulb)
 library.add(faSpinner)
+library.add(faFileImport)
+library.add(faFileExport)
+library.add(faTrash)
 
 const app = createApp(App)
 app.use(router)
