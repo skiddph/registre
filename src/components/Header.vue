1708dba6e414b8d63634b43c56a16b87a02d2322
eru123 <yeoligoakino@gmail.com>
2022-03-02 17:43:25 +0800

    implement data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,17 @@
 <script setup>
+import { ref } from 'vue'
 import { useStore } from 'vuex'
+import DataExport from '@/components/DataExport.vue'
 const store = useStore()
+
+const fields = [ 'user', 'unit', 'position', 'office', 'logs', 'employee' ]
+
+const dataExport = ref(true)
+const dataImport = ref(false)
+const dataReset = ref(null)
 </script>
 <template>
+  <DataExport :fields="fields" :open="dataExport" @close="dataExport = false" />
   <div v-if="!store.state.token" class="navbar-container">
     <div class="navbar">
       <router-link class="brand" to="/">{{ store.state.app.name || 'Logbook' }}</router-link>
@@ -16,6 +25,20 @@ const store = useStore()
     <div class="navbar">
       <router-link class="brand" to="/">{{ store.state.app.name || 'Logbook' }}</router-link>
       <div class="spacer"></div>
+      <button class="link">
+        <span>Data</span>
+        <div class="items">
+          <button @click="dataExport = true">
+            <icon icon="file-export" />Backup
+          </button>
+          <button @click="dataImport = true">
+            <icon icon="file-import" />Restore
+          </button>
+          <button @click="dataReset = true">
+            <icon icon="trash" />Reset
+          </button>
+        </div>
+      </button>
       <router-link v-if="store.state.role == 1" class="link" to="/superadmin">DASHBOARD</router-link>
       <router-link v-if="store.state.role != 1" class="link" to="/admin">DASHBOARD</router-link>
       <router-link class="link" to="/logs">LOGS</router-link>
@@ -24,7 +47,7 @@ const store = useStore()
   </div>
 </template>
 <style lang="scss" scoped>
-@import '@/styles/variables.scss';
+@import "@/styles/variables.scss";
 
 .navbar-container {
   @apply w-full flex flex-col items-center justify-center shadow sticky top-0 bg-white z-10;
@@ -42,6 +65,21 @@ const store = useStore()
       font-family: Arial, Helvetica, sans-serif;
       &.router-link-active {
         @apply hidden;
+      }
+
+      .items {
+        @apply absolute bg-white border rounded flex-col justify-center shadow-lg z-10 hidden;
+
+        button {
+          svg {
+            @apply mr-2;
+          }
+          @apply py-2 px-4 hover:bg-gray-200 text-gray-600 hover:text-gray-900 text-left;
+        }
+      }
+
+      &:hover .items {
+        @apply flex;
       }
     }
 
