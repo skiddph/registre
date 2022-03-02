9e6c5c1184271815bf485f42c477b001b7d4fa9f
eru123 <yeoligoakino@gmail.com>
2022-03-02 13:07:38 +0800

    apply button hover effect

--- src/views/logs.vue
+++ src/views/logs.vue
@@ -179,7 +179,7 @@ const minutes = (() => {
       @apply w-full flex flex-row items-center justify-end my-4;
 
       button {
-        @apply px-4 py-1 text-sm text-white bg-blue-500 rounded-full;
+        @apply px-4 py-1 text-sm text-white bg-blue-500 hover:bg-blue-700 rounded-full transition duration-300 ease-in-out;
       }
     }
   }
