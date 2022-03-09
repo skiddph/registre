fcecd1de595bacdbbdf409d6d7d40741c20b257a
eru123 <yeoligoakino@gmail.com>
2022-03-09 15:55:55 +0800

    fix top level import node-v15

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { useStore } from 'vuex'
-const store = useStore()
-store.dispatch('usercount')
 import Header from '@/components/Header.vue'
 import Main from '@/components/Main.vue'
+const store = useStore()
+store.dispatch('usercount')
 </script>
 <template>
   <Header />
