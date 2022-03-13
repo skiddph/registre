4536ad20c92a2d704861419cdc61bfebab35ef7b
eru123 <yeoligoakino@gmail.com>
2022-03-14 06:33:05 +0800

    added release job

---
+++ release.js
@@ -0,0 +1,40 @@
+const { exec } = require("child_process");
+const fs = require('fs-extra')
+const path = require('node:path');
+
+
+const rootDir = process.cwd();
+
+const build = async (cb) => {
+  const cl = exec(`cd ${rootDir}/client && npm run build`)
+  cl.stdout.on('data', (data) => console.log(data))
+  cl.stderr.on('data', (data) => console.log(data))
+  cl.on('close', (code) => createRelease())
+}
+
+const createRelease = async (cb) => {
+  const exclude = [ '.gitignore', 'node_modules', 'certs' ]
+
+  // copy server files to release folder except for excluded files recursively
+  try {
+    fs.copySync(path.join(rootDir, 'server'), path.join(rootDir, 'release','registre'), {
+      filter: (src) => {
+        return exclude.indexOf(path.basename(src)) === -1
+      }
+    })
+    archiveRelease()
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const archiveRelease = async (cb) => {
+  const fromF = path.join(rootDir, 'release', 'registre')
+  const toF = path.join(rootDir, 'release', 'registre.zip')
+  const archive = exec(`powershell Compress-Archive -Path ${fromF} -DestinationPath ${toF}`)
+  archive.stdout.on('data', (data) => console.log(data))
+  archive.stderr.on('data', (data) => console.log(data))
+  archive.on('close', (code) => console.log(`child process exited with code ${code}`))
+}
+
+build()
