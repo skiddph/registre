71a5c5d2669b7023d0b9e4ef30c3ea1cddb67f45
eru123 <yeoligoakino@gmail.com>
2022-03-16 15:23:59 +0800

    added api v2 entry

---
+++ server/src/plugins/api-v2/index.js
@@ -0,0 +1,18 @@
+const fp = require('fastify-plugin')
+
+const system = require('./system')
+const user = require('./user')
+
+const v2 = fp(async (app, opts, done) => {
+  opts.base_url = opts.base_url || "/api/v2"
+
+  await app.register(user, opts)
+  await app.register(system, opts)
+
+  done()
+}, {
+  name: 'registre-api-v2',
+  fastify: '3.x'
+})
+
+module.exports = v2
