f5b8bdf910b85debd9d6721677d55828d23da2c6
eru123 <yeoligoakino@gmail.com>
2022-03-16 16:14:05 +0800

    implement employee api

--- server/src/plugins/api-v2/index.js
+++ server/src/plugins/api-v2/index.js
@@ -2,12 +2,14 @@ const fp = require('fastify-plugin')
 
 const system = require('./system')
 const user = require('./user')
+const employee = require('./employee')
 
 const v2 = fp(async (app, opts, done) => {
   opts.base_url = opts.base_url || "/api/v2"
 
   await app.register(user, opts)
   await app.register(system, opts)
+  await app.register(employee, opts)
 
   done()
 }, {
