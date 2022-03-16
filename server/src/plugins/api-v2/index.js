3fec09242ed801b9f032c28f0d20c92031c88c70
eru123 <yeoligoakino@gmail.com>
2022-03-16 19:57:05 +0800

    implement log api v2

--- server/src/plugins/api-v2/index.js
+++ server/src/plugins/api-v2/index.js
@@ -3,6 +3,7 @@ const fp = require('fastify-plugin')
 const system = require('./system')
 const user = require('./user')
 const employee = require('./employee')
+const log = require('./log')
 
 const v2 = fp(async (app, opts, done) => {
   opts.base_url = opts.base_url || "/api/v2"
@@ -10,6 +11,7 @@ const v2 = fp(async (app, opts, done) => {
   await app.register(user, opts)
   await app.register(system, opts)
   await app.register(employee, opts)
+  await app.register(log, opts)
 
   done()
 }, {
