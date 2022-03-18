2a3aa8d5a43bb2bfdeb9c829c7d59b27eb51c997
eru123 <yeoligoakino@gmail.com>
2022-03-18 13:47:21 +0800

    fix bugs

--- server/src/lib/log2.js
+++ server/src/lib/log2.js
@@ -8,14 +8,17 @@ const transformEmployeeData = async (app, data) => {
   })
     .then(e => JSON.parse(e.value))
   
+  data.has_schedule = true
+
   if(!data.data?.schedule){
+    data.has_schedule = false
     data.data.schedule = await app.prisma.system.findUnique({
       where: {
         key: 'fallback_schedule'
       }
     })
       .then(e => JSON.parse(e.value))
-      .catch(() => []);
+      .catch(() => [0, 0]);
   }
 
   for (let field of dropdown_fields) {
@@ -23,7 +26,7 @@ const transformEmployeeData = async (app, data) => {
   }
 
   // if schedule field is empty, set it to fallback schedule
-  if (!data.schedule) {
+  if (!data.schedule && data.has_schedule) {
     data.schedule = data.data.schedule
   }
 
