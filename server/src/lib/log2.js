0b34611af94029629b1fb985e695c59eb42270ed
eru123 <yeoligoakino@gmail.com>
2022-03-18 17:36:46 +0800

    added transformLogData and transformLogData

--- server/src/lib/log2.js
+++ server/src/lib/log2.js
@@ -1,4 +1,8 @@
+const _ = require('lodash')
+
 const transformEmployeeData = async (app, data) => {
+  if(!data) return 
+
   data.data = typeof data.data === 'string' ? JSON.parse(data.data) : data.data
 
   const dropdown_fields = await app.prisma.system.findUnique({
@@ -7,10 +11,10 @@ const transformEmployeeData = async (app, data) => {
     }
   })
     .then(e => JSON.parse(e.value))
-  
+
   data.has_schedule = true
 
-  if(!data.data?.schedule){
+  if (!data.data?.schedule) {
     data.has_schedule = false
     data.data.schedule = await app.prisma.system.findUnique({
       where: {
@@ -18,7 +22,7 @@ const transformEmployeeData = async (app, data) => {
       }
     })
       .then(e => JSON.parse(e.value))
-      .catch(() => [0, 0]);
+      .catch(() => [ 0, 0 ]);
   }
 
   for (let field of dropdown_fields) {
@@ -31,11 +35,85 @@ const transformEmployeeData = async (app, data) => {
   }
 
   delete data.data
-  console.log('log2data',data)
+  console.log('log2data', data)
   return data
 }
 
+const transformLogData = (data) => {
+  /*
+    where data = [
+      {
+        id: Number,
+        timestamp: Date,
+        data: Object, // JSON
+        employee: String,
+      }
+    ]
+  */
+
+  // group by date
+  const groupedByDate = _.groupBy(data, (e) => {
+    return new Date(e.timestamp).setHours(0, 0, 0, 0)
+  })
+
+  const groupedByEmployee = Object.create(null)
+  const result = []
+
+  for (let date in groupedByDate) {
+    const logs = groupedByDate[ date ]
+
+    // group by employee
+    groupedByEmployee[ date ] = _.groupBy(logs, (e) => {
+      return e.employee
+    })
+
+    // group stamps by employee
+    for (let employee in groupedByEmployee[ date ]) {
+      const log_code = {
+        AM_IN: null,
+        AM_OUT: null,
+        PM_IN: null,
+        PM_OUT: null,
+      }
+      let log_f = {}
+      const log_r = _.sortBy(groupedByEmployee[ date ][ employee ], 'id', 'asc').map(e => {
+        e.data = typeof e.data === 'string' ? JSON.parse(e.data) : e.data
+        log_code[ `${e.data.time_code}_${e.data.time_sigm}` ] = e.timestamp
+        log_f = {
+          ...log_f,
+          ..._.omit(e, [ 'data', 'timestamp', 'id' ]),
+          ..._.omit(e.data, [ 'time_code', 'time_sigm', 'schedule' ]),
+        }
+      })
+
+      log_f.timestamps = log_code
+      result.push(log_f)
+    }
+  }
+
+  return result
+}
+
+const transformReportData = (data) => {
+  logs = transformLogData(data)
+  const result = Object.create(null)
+  console.log('logs', logs)
+  logs.forEach(e => {
+    const remarks = e.remarks || false
+    console.log('log2result', result)
+    result[ e.employee ] = _.omit(e, [ 'remarks', 'timestamps', 'has_schedule' ])
+    if(!result[ e.employee ].hasOwnProperty('remarks')) result[ e.employee ].remarks = {}
+    if(remarks) {
+      // increment remarks
+      result[ e.employee ].remarks[remarks] = result[ e.employee ].remarks[remarks] ? result[ e.employee ].remarks[remarks] + 1 : 1
+    }
+  })
+
+  return Object.values(result)
+}
 
 module.exports = {
-  transformEmployeeData
+  transformEmployeeData,
+  transformLogData,
+  transformReportData,
 }
