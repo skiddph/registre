3af4f0a33acd7e62b2ba4d040d63ac5161aeb421
eru123 <yeoligoakino@gmail.com>
2022-03-20 18:18:55 +0800

    update restore

--- server/src/plugins/api-v2/data.js
+++ server/src/plugins/api-v2/data.js
@@ -4,7 +4,11 @@ const fs = require('node:fs')
 
 const plugins = fp(async (app, opts, done) => {
 
-  let { base_url, public = path.join(process.cwd(), 'public'), tmp = path.join(process.cwd(), 'tmp') } = opts
+  let {
+    base_url,
+    public = path.join(process.cwd(), 'public'),
+    tmp = path.join(process.cwd(), 'tmp')
+  } = opts
 
   const dowload = path.join(public, 'download')
   const upload = path.join(tmp, 'upload')
@@ -50,6 +54,24 @@ const plugins = fp(async (app, opts, done) => {
       code: 'DE002',
       message: 'Backup must have atleast 1 valid table'
     },
+    'DE003': {
+      status: 'error',
+      code: 'DE003',
+      message: 'Restore file not found'
+    },
+  }
+
+  const SUCCESS_CODE = {
+    'DS001': {
+      status: 'success',
+      code: 'DS001',
+      message: 'Backup created successfully'
+    },
+    'DS002': {
+      status: 'success',
+      code: 'DS002',
+      message: 'Restore created successfully'
+    },
   }
 
   const VALID_BACKUP_FIELDS = [
@@ -92,41 +114,88 @@ const plugins = fp(async (app, opts, done) => {
       data
     }, null, 2)
 
-
-
     // write data to file
     const filename = `backup-${Date.now()}.json`
     const filepath = path.join(dowload, filename)
     fs.writeFileSync(filepath, data)
     return res.code(200).send({
+      ...SUCCESS_CODE[ 'DS001' ],
       link: '/download/' + filename
     })
   })
 
   // data restore where they upload the file
   app.post(`${base_url}/data/restore`, async (req, res) => {
+
     // get file
-    if(!req.body?.file?.tempFilePath){
+    if (!req.body?.file?.tempFilePath) {
       return res.code(403).send(ERROR_CODE[ 'DE001' ])
     }
 
-    // get file
-    const file = req.body?.file?.tempFilePath
+    const overwrite = req.body?.overwrite || true
 
     // check if a valid json
     let data;
     try {
-      let data = fs.readFileSync(file)
+      let data = fs.readFileSync(req.body?.file?.tempFilePath)
       data = JSON.parse(data)
-    } catch (error) {
-      return res.code(403).send(ERROR_CODE[ 'DE001' ])
+      if (data) {
+        let version = data.version || 1
+        if (version == 2) {
+          data = JSON.parse(Buffer.from(data.data, 'base64').toString())
+          const fail = {}
+          const total = {
+            system: 0,
+            user: 0,
+            employee: 0,
+            logs: 0
+          }
+
+          for (let table of Object.keys(total)) {
+            if (typeof data[ table ] !== 'undefined') {
+              for (let row of data[ table ]) {
+                total[ table ]++
+
+                const query = overwrite ? {
+                  where: {
+                    ...(() => table == 'system' ? { key: row.key } : { id: row.id })()
+                  },
+                  update: row,
+                  create: row
+                } : {
+                  data: row
+                }
+
+                await app.prisma[ table ][ overwrite ? 'upsert' : 'create' ](query)
+                  .catch((e) => {
+                    console.log(table, row.id, e.code)
+                    if (table == 'logs') {
+                      console.log(e)
+                    }
+                    fail[ table ] = fail[ table ] ? fail[ table ] + 1 : 1
+                  })
+              }
+            }
+          }
+
+          return res.code(200).send({
+            ...SUCCESS_CODE[ 'DS002' ],
+            data: {
+              ...((t, f) => {
+                const d = {}
+                for (let tb of Object.keys(t)) d[ tb ] = `${t[ tb ] - (f[ tb ] || 0)} of ${t[ tb ]}`
+                return d
+              })(total, fail)
+            }
+          })
+        } else throw 'Invalid data version'
+      } else throw 'Invalid data'
+    } catch (e) {
+      console.log(e)
+      return res.code(403).send(ERROR_CODE[ 'DE003' ])
     }
 
-    // check if data is valid
-    if (!data){
 
-    }
-    
   })
 
   done()
