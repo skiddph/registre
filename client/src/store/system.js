90980212ec3fa84d20df964072c7ebe0a6cd7281
eru123 <yeoligoakino@gmail.com>
2022-03-21 11:35:13 +0800

    system module

---
+++ client/src/store/system.js
@@ -0,0 +1,93 @@
+import { createMutations, createResetAction } from './lib'
+const DEFAULT_STATE = {
+  dropdown_fields: [],
+  foo: {}
+}
+const module = {
+  namespaced: true,
+  state: () => DEFAULT_STATE,
+  mutations: {
+    ...createMutations(DEFAULT_STATE),
+    // addtional mutations here
+  },
+  actions: {
+    reset: createResetAction(DEFAULT_STATE),
+    async get({ commit, rootState }, useCredentials = false) {
+      return await fetch(`${rootState.api_url}/system`, {
+        method: 'GET',
+        headers: {
+          ...(useCredentials ? { 'Authorization': `Bearer ${rootState.user.token}`, } : {}),
+        }
+      })
+        .then(res => res.json())
+        .then(res => {
+          for (let key in res.data) {
+            try {
+              res.data[ key ] = res.data[ key ]
+              if (DEFAULT_STATE[ key ]) {
+                commit(key, res.data[ key ])
+              }
+            } catch (e) {
+              console.log(`[System] Failed to add ${key} to state.`)
+            }
+          }
+          return res
+        })
+    },
+    async upsert({ commit, rootState }, { key, value, role = null }) {
+      // key must be a string and value must be an object
+      if (typeof key !== 'string' || typeof value !== 'object') {
+        return {
+          status: 'error',
+          code: 'SE005',
+          message: "'key' must be a string and 'value' must be an object"
+        }
+      }
+
+      value = JSON.stringify(value)
+      return await fetch(`${rootState.api_url}/system`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${rootState.user.token}`,
+        },
+        body: JSON.stringify({ key, value, role }),
+      })
+        .then(res => res.json())
+        .then(res => {
+          if (DEFAULT_STATE[ key ]) {
+            commit(res.data.key, JSON.parse(res.data.value))
+          }
+          return res
+        })
+    },
+    async delete({ commit, rootState }, key) {
+      // key must be a string
+      if (typeof key !== 'string') {
+        return {
+          status: 'error',
+          code: 'SE006',
+          message: "'key' must be a string"
+        }
+      }
+
+      return await fetch(`${rootState.api_url}/system`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${rootState.user.token}`,
+        },
+        body: JSON.stringify({ key}),
+      })
+        .then(res => res.json())
+        .then(res => {
+          if (DEFAULT_STATE[ key ]) {
+            commit(key, DEFAULT_STATE[ key ])
+          }
+          return res
+        })
+    }
+  }
+}
+
+export default module
