a0a01d3176be3daaaf626c1c71c20dfab49f0aa8
eru123 <yeoligoakino@gmail.com>
2022-03-21 13:53:42 +0800

    user user module

--- client/src/layouts/default.vue
+++ client/src/layouts/default.vue
@@ -3,7 +3,7 @@ import { useStore } from 'vuex'
 import Header from '@/components/Header.vue'
 import Main from '@/components/Main.vue'
 const store = useStore()
-store.dispatch('usercount')
+store.dispatch('user/get')
 </script>
 <template>
   <Header />
