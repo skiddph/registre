2dedf9430be3d78ea4a07adead154e28251e07ac
eru123 <yeoligoakino@gmail.com>
2022-03-21 16:47:51 +0800

    remove unused

--- client/src/components/AdminDashboard.vue
+++ client/src/components/AdminDashboard.vue
@@ -1,16 +1,8 @@
 <script setup>
-import { useStore } from 'vuex'
-import { watchEffect } from 'vue'
 import TabbedMenu from './TabbedMenu.vue'
 import DashboardActions from './DashboardActions.vue'
 import AddForm from './AddForm.vue'
 import SearchResult from './SearchResult.vue'
-
-const store = useStore()
-
-watchEffect(() => {
-  store.dispatch('getList')
-})
 </script>
 <template>
   <div class="dashboard-container">
