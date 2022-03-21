c764f4eac2bf4826a6c5e8033e1c75654c1a63ea
eru123 <yeoligoakino@gmail.com>
2022-03-21 11:18:09 +0800

    added store helper library

---
+++ client/src/store/lib.js
@@ -0,0 +1,17 @@
+export const createMutations = (state) => {
+  const mutations = {}
+  for (let key in state) {
+    mutations[key] = (state, payload) => {
+      state[key] = payload
+    }
+  }
+  return mutations
+}
+
+export const createResetAction = (state) => {
+  return ({commit}) => {
+    Object.keys(state).forEach(key => {
+      commit(key, state[key]) 
+    })
+  }
+}
