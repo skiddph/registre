eb935db62b40b9158883237c59a83870f6410728
eru123 <yeoligoakino@gmail.com>
2022-03-21 17:39:32 +0800

    update

--- client/src/store/lib.js
+++ client/src/store/lib.js
@@ -5,13 +5,26 @@ export const createMutations = (state) => {
       state[ key ] = payload
     }
   }
+  mutations.set = (state, {key, value}) => {
+    state[ key ] = value
+  }
+
+  mutations.delete = (state, key) => {
+    delete state[ key ]
+  }
   return mutations
 }
 
-export const createResetAction = (state) => {
-  return ({ commit }) => {
+export const createResetAction = (DEFAULT_STATE) => {
+  return ({ commit, state}) => {
+    Object.keys(DEFAULT_STATE).forEach(key => {
+      commit(key, DEFAULT_STATE[ key ])
+    })
+
     Object.keys(state).forEach(key => {
-      commit(key, state[ key ])
+      if (!DEFAULT_STATE[ key ]) {
+        commit('delete', key)
+      }
     })
   }
 }
