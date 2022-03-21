c1993ce158d21cdc23eb9af4242d38cb9bf82664
eru123 <yeoligoakino@gmail.com>
2022-03-21 13:27:07 +0800

    added login, update and delete

--- client/src/store/user.js
+++ client/src/store/user.js
@@ -2,7 +2,7 @@ import { createMutations, createResetAction } from './lib'
 import _ from 'lodash'
 const DEFAULT_STATE = {
   users: [],
-  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjo4LCJpYXQiOjE2NDc0MDkwODF9.8MsgflWsKiLteotLWtNHF0wtsFQEVxpWJiLUa1e9Tuo",
+  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoxNCwiaWF0IjoxNjQ3ODM3MTUxfQ.5fHBtfly0slieNefANrcdUUIIgjOMAB2Vs8djHexmGs",
 }
 
 const module = {
@@ -25,9 +25,57 @@ const module = {
       })
         .then(e => e.json())
         .then(e => {
-          commit('users', [ ...state.users, _.omit(e.data, [ 'hash' ]) ])
+          if (!state.token) commit('token', e.token || "")
+          if (e.data) commit('users', [ ...state.users, _.omit(e.data, [ 'hash' ]) ])
+          return e
         })
     },
+    async login({ commit, rootState, state }, { user, pass }) {
+      return await fetch(`${rootState.api_url}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user, pass })
+      })
+        .then(e => e.json())
+        .then(e => {
+          commit('token', e.token || "")
+          console.log(state.token)
+          return e
+        })
+    },
+    async update({ commit, rootState, state }, { id, user, name, role, pass }) {
+      return await fetch(`${rootState.api_url}/user/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${state.token}`
+        },
+        body: JSON.stringify({ user, name, role, pass })
+      })
+        .then(e => e.json())
+        .then(e => {
+          if (e.token) commit('token', e.token || "")
+          if (e.data) commit('users', _.map(state.users, (e) => e.id == id ? _.omit(e, [ 'hash' ]) : e))
+          return e
+        })
+    },
+    async delete({ commit, rootState, state }, id) {
+      return await fetch(`${rootState.api_url}/user/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${state.token}`
+        },
+      })
+        .then(e => e.json())
+        .then(e => {
+          if (e.token) commit('token', e.token || "")
+          if (e.data) commit('users', _.filter(state.users, (e) => e.id != id))
+          return e
+        })
+    }
   }
 }
 
