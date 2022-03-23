db19371c96bbe1e38490396ecc920d7b87268042
eru123 <yeoligoakino@gmail.com>
2022-03-24 07:43:22 +0800

    added store

--- client/src/components/AdminDashboard.vue
+++ client/src/components/AdminDashboard.vue
@@ -3,12 +3,15 @@ import TabbedMenu from './TabbedMenu.vue'
 import DashboardActions from './DashboardActions.vue'
 import AddForm from './AddForm.vue'
 import SearchResult from './SearchResult.vue'
+import { useStore } from 'vuex'
+const store = useStore()
 </script>
 <template>
   <div class="dashboard-container">
     <TabbedMenu />
     <DashboardActions />
-    <AddForm />
+
+    <!-- <AddForm /> -->
     <SearchResult />
   </div>
 </template>
