13ef3ff19db9f691b0dfc87e13058797c65555f6
eru123 <yeoligoakino@gmail.com>
2022-03-24 07:44:56 +0800

    added search and result

--- client/src/store/dashboard.js
+++ client/src/store/dashboard.js
@@ -3,7 +3,12 @@ import _ from 'lodash'
 const DEFAULT_STATE = {
   tabs: [],
   data: {},
-  active: 'employees',
+  active: 'overview',
+  search: "",
+  result_loading: false,
+  result: [],
+  result_headers: [],
+  addform: false
 }
 
 const module = {
@@ -15,8 +20,33 @@ const module = {
   },
   actions: {
     reset: createResetAction(DEFAULT_STATE),
-    active({ commit }, tab) {
+    active({ commit, dispatch }, tab) {
       commit('active', String(tab).toLowerCase())
+      dispatch('search', "")
+    },
+    search({ commit, state }, search) {
+      commit('search', search)
+      let result = _.filter(state.data[ state.active ], (item) => {
+        if (!search) return true
+        if (typeof item != "string") {
+          let hasMatch = false
+          Object.keys(item).forEach(f => {
+            if (!hasMatch)
+              hasMatch = typeof item[ f ] == "string" ?
+                _.includes(String(item[ f ]).toLowerCase(), search.toLowerCase()) :
+                _.includes(item[ f ], search)
+          })
+          return hasMatch
+        } else {
+          return _.includes(String(item).toLowerCase(), search.toLowerCase())
+        }
+      })
+      commit('result', result)
+      if (typeof result[ 0 ] == "string") {
+        commit('result_headers', [ "value" ])
+      } else {
+        commit('result_headers', _.keys(result[ 0 ]))
+      }
     },
     async data({ commit, state, rootState, dispatch }, key) {
       if (key == 'admins') {
@@ -24,6 +54,16 @@ const module = {
       } else if (key == 'employees') {
         await dispatch('employee/get', null, { root: true })
         commit('data', { ..._.omit(state.data, [ key ]), employees: rootState.employee.employees })
+      } else if (key == 'overview') {
+        await dispatch('logs/report', null, { root: true })
+        const data = _.map(rootState.logs.report, (item) => {
+          return {
+            ..._.omit(item, ['remarks']),
+            undertime: item.remarks?.undertime || 0,
+            late: item.remarks?.late || 0,
+          }
+        })
+        commit('data', { ..._.omit(state.data, [ key ]), overview: data})
       } else if (rootState.system[ key ]) {
         commit('data', { ..._.omit(state.data, [ key ]), [ key ]: rootState.system[ key ] })
       } else {
