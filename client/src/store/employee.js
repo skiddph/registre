f43b5096e8bebf81ca7bb6d8839d54fc02277712
eru123 <yeoligoakino@gmail.com>
2022-03-24 23:19:37 +0800

    added create

--- client/src/store/employee.js
+++ client/src/store/employee.js
@@ -18,14 +18,14 @@ const module = {
         headers: {
           'Authorization': `Bearer ${rootState.user.token}`
         }
-      }) 
+      })
         .then(e => e.json())
         .then(e => {
           if (e.data) commit('employees', e.data)
           return e
         })
     },
-    delete({ commit, rootState, state}, id) {
+    delete({ commit, rootState, state }, id) {
       return fetch(`${rootState.api_url}/employee/${id}`, {
         method: 'DELETE',
         headers: {
@@ -35,6 +35,39 @@ const module = {
         .then(e => e.json())
         .then(e => {
           if (e.data) commit('employees', _.filter(state.employees, { id: e.data.id }))
+          return e
+        })
+    },
+    async create({ commit, rootState, state }, formdata) {
+      return await fetch(`${rootState.api_url}/employee`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${rootState.user.token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formdata)
+      })
+        .then(e => e.json())
+        .then(e => {
+          if (e.data) commit('employees', [ ...state.employees, _.omit(e.data, [ 'hash' ]) ])
+          return e
+        })
+    },
+    async update({ commit, rootState, state }, formdata) {
+      return await fetch(`${rootState.api_url}/employee/${formdata.id}`, {
+        method: 'PUT',
+        headers: {
+          'Authorization': `Bearer ${rootState.user.token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(_.omit(formdata, [ 'id' ]))
+      })
+        .then(e => e.json())
+        .then(e => {
+          if (e.data) commit('employees', _.map(state.employees, (e) => {
+            if (e.id === formdata.id) return _.omit(e.data, [ 'hash' ])
+            return e
+          }))
           return e
         })
     }
