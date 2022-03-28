32263b370265d6383a56c7dbbebc570712d79f59
eru123 <yeoligoakino@gmail.com>
2022-03-28 11:52:19 +0800

    node 15 compat

--- server/src/plugins/api-v2/data.js
+++ server/src/plugins/api-v2/data.js
@@ -1,6 +1,6 @@
 const fp = require('fastify-plugin')
-const path = require('node:path')
-const fs = require('node:fs')
+const path = require('path')
+const fs = require('fs')
 
 const plugins = fp(async (app, opts, done) => {
 
