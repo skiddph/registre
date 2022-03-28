2d47701acd6e05e77c84822381133cd5db580744
eru123 <yeoligoakino@gmail.com>
2022-03-28 14:53:33 +0800

    initDarkMode

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,11 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useStore } from 'vuex'
+const store = useStore()
+
+onMounted(() => store.dispatch('darkModeInit'))
+
+</script>
 <template>
   <router-view />
 </template>
