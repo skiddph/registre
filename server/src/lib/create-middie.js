40ede588e9d59e29456e8e5e05c1114960d6c602
eru123 <yeoligoakino@gmail.com>
2022-04-01 09:01:49 +0800

    fix remarks bug

--- server/src/lib/create-middie.js
+++ server/src/lib/create-middie.js
@@ -6,16 +6,19 @@ const defaultMiddie = (app, req, res, ERROR_CODE, SUCCESS_CODE) => {
   const create = async (employee) => {
     // get current date and time
     const date = new Date()
-    const date_begin = new Date(date.setHours(0, 0, 0, 0))
-    const date_end = new Date(date.setHours(23, 59, 59, 999))
-    const time = date.getTime()
+    const date_begin = new Date(new Date(date.getTime()).setHours(0, 0, 0, 0))
+    const date_end = new Date(new Date(date.getTime()).setHours(23, 59, 59, 999))
+    const time = new Date().getTime()
 
     // get schedule from employee and parse
-    // employee.schedule = "08:00 AM - 12:00 PM"
     const schedule = employee.schedule.split('-').map(time => {
-      time = time.trim()
-      return new Date(new Date(parseInt(time)).setDate(new Date().getDate()))
+      time = new Date(parseInt(time.trim()))
+      return new Date().setHours(time.getHours(), time.getMinutes(), time.getSeconds(), time.getMilliseconds())
     })
+
+    console.log(format(schedule[0], 'HH:mm'))
+    console.log(format(schedule[1], 'HH:mm'))
+    console.log(format(new Date(), 'HH:mm'))
 
     // get all user logs from with employee id and begin date and end date
     const userTodayLogs = await app.prisma.logs.findMany({
@@ -41,49 +44,50 @@ const defaultMiddie = (app, req, res, ERROR_CODE, SUCCESS_CODE) => {
     // get time code from date (AM|PM)
     employee.time_code = format(new Date(), 'a')
 
+    // employee log remarks
+    employee.log_remarks = []
+    
     if (employee.has_schedule && userTodayLogs.length === 0 && employee.time_code === 'AM') {
       // AM RANGE BASE ON EMPLOYEE SCHEDULE
-      const AM = new Date(schedule[ 0 ] && schedule[ 0 ] > 0 ? set(schedule[ 0 ], {
-        year: date.getFullYear(),
-        month: date.getMonth(),
-        day: date.getDate()
-      }) : (new Date(Date.now())).setHours(0, 0, 0, 0))
+      const AM = schedule[0] ? new Date(new Date(schedule[0]).setDate(new Date().getDate())) : new Date(new Date().setHours(0, 0, 0, 0))
 
       // PM RANGE BASE ON EMPLOYEE SCHEDULE
-      const PM = new Date(schedule[ 1 ] && schedule[ 1 ] > 0 ? set(schedule[ 1 ], {
-        year: date.getFullYear(),
-        month: date.getMonth(),
-        day: date.getDate()
-      }) : (new Date(Date.now())).setHours(23, 59, 59, 999))
+      const PM = schedule[1] ? new Date(new Date(schedule[1]).setDate(new Date().getDate())) : new Date(new Date().setHours(23, 59, 59, 999))
 
       // get grace period, in minutes
-      const grace_period = await app.prisma.system.findUnique({
-        where: {
-          key: 'grace_period'
-        }
-      })
-        .then(e => JSON.parse(e.value))
-        .catch(() => 15)
+      // 15mins to timestamp
+      const grace_range = [
+        AM.getTime(),
+        // add 15 mins
+        set(AM, { minutes: AM.getMinutes() + 15 }).getTime()
+      ]
 
-      // check if date is between AM and AM + grace period
-      if (time >= AM.getTime() && time <= (AM.getTime() + grace_period * 60000)) {
-        employee.morning_remarks = 'grace_period'
-      }
-
-      // check if early in
-      if (time < AM.getTime()) {
-        employee.morning_remarks = 'early'
+      // check if time is in grace range
+      if (time >= grace_range[0] && time <= grace_range[1]) {
+        employee.log_remarks.push('grace')
       }
 
       // else if late
-      else if (time > PM.getTime()) {
-        employee.morning_remarks = 'late'
+      else if (time > grace_range[1]) {
+        employee.log_remarks.push('late')
+      }
+
+      // else if early
+      else if (time < grace_range[0]) {
+        employee.log_remarks.push('early')
+      }
+
+      // else if on time
+      else {
+        employee.log_remarks.push('error')
       }
     }
 
     // iterate through user logs
     let UAML = 0 // User AM Logs
     let UPML = 0 // User PM Logs
+
+    // 
     for (let log of userTodayLogs) {
       // get log time
       if (log.time_code === 'AM') {
